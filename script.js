13dde08aeb24458d77f2520c96a4f81a50e74d0d
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-07 13:06:11 +0600

    added the load trees function and fetched the data from api and show it to the display

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
 const categoryContainer = document.getElementById("category-container");
+const treesContainer = document.getElementById("trees-container");
 
 const loadCategories = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/categories");
-    const data = await res.json();
-    data.categories.forEach(category => {
-        const btn = document.createElement('button');
-        btn.className = "btn btn-success w-full justify-start btn-outline";
-        btn.textContent = category.category_name;
-        categoryContainer.append(btn);
-    });
-}
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/categories",
+  );
+  const data = await res.json();
+  data.categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.className = "btn btn-success w-full justify-start btn-outline";
+    btn.textContent = category.category_name;
+    categoryContainer.append(btn);
+  });
+};
+
+const loadTrees = async () => {
+  const res = await fetch("https://openapi.programming-hero.com/api/plants");
+  const data = await res.json();
+  displayTrees(data.plants);
+};
+
+// category
+// :
+// "Fruit Tree"
+// description
+// :
+// "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals."
+// id
+// :
+// 1
+// image
+// :
+// "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg"
+// name
+// :
+// "Mango Tree"
+// price
+// :
+// 500
+const displayTrees = (trees) => {
+  console.log(trees);
+  trees.forEach((tree) => {
+    const div = document.createElement("div");
+    div.className = "card bg-base-100 shadow-sm";
+    div.innerHTML = `
+    <figure>
+              <img
+                src="${tree.image}"
+                class="h-45 w-full object-cover rounded-t-lg"
+              />
+            </figure>
+            <div class="card-body p-3 space-y-1">
+              <h2 class="card-title">${tree.name}</h2>
+              <p class="line-clamp-2">
+                ${tree.description}
+              </p>
+              <div class="flex justify-between items-center">
+                <div class="badge badge-soft badge-success">${tree.category}</div>
+                <div class="font-semibold">$${tree.price}</div>
+              </div>
+              <div class="card-actions justify-between items-center">
+                <button class="btn btn-success w-full rounded-full">Add to Cart</button>
+              </div>
+            </div>
+    `;
+    treesContainer.append(div);
+  });
+};
+
 loadCategories();
+loadTrees();
